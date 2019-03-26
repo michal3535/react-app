3b764339b88364ae5f5dba96b77f0774e9ba70d2
Michał Wlazło <michal3535@poczta.fm>
2019-03-26 21:53:04 +0100

    Updates

---
+++ precache-manifest.b2c744876ed67f138a27bf4510cc6924.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "a620c640c568e131556b",
+    "url": "/react-app/static/js/main.c04a461b.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/react-app/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "a620c640c568e131556b",
+    "url": "/react-app/static/css/main.c340f93d.chunk.css"
+  },
+  {
+    "revision": "ccef018d88c2ca091929cb3c685ddff6",
+    "url": "/react-app/index.html"
+  }
+];
